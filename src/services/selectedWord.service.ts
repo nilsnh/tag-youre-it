9ce335bd0effb66a70965f5c2c5be96c6c20201d
Nils Norman Haukås <nils@thunki.com>
2015-11-14 13:35:47 +0100

    Revert "Added mocha dependency for testing."
    This reverts commit 7ae65ed402f10cb19437d7b754d01e8f99066d25.

--- src/services/selectedWord.service.ts
+++ src/services/selectedWord.service.ts
@@ -40,42 +40,42 @@ export class SelectedWordService {
     }
   }
 
-  // place spans around a tagged word.
-  addTagToPage (sense : ISense) {
-    var windowSelection = window.getSelection();
-    var range = this.currentSelectionRange;
-    var span : HTMLSpanElement = document.createElement('span');
-    span.id = sense.senseid;
-    span.title = sense.explanation;
-    span.className = 'tagit-tag';
-    range.surroundContents(span);
-    windowSelection.removeAllRanges();
-    // windowSelection.addRange(range);
-    this.$log.debug('addTagToPage');
-  }
+    // place spans around a tagged word.
+    addTagToPage (sense : ISense) {
+      var windowSelection = window.getSelection();
+      var range = this.currentSelectionRange;
+      var span : HTMLSpanElement = document.createElement('span');
+      span.id = sense.senseid;
+      span.title = sense.explanation;
+      span.className = 'tagit-tag';
+      range.surroundContents(span);
+      windowSelection.removeAllRanges();
+      // windowSelection.addRange(range);
+      this.$log.debug('addTagToPage');
+    }
 
-  private findSelection () {
-    var focused : any = document.activeElement;
-    var selectedText : string;
-    // try grabbing text from an input or textarea field
-    // commenting this until we need to figure out tagging of editable fields
-    // if (focused) {
-    //   try {
-    //     selectedText = focused.value.substring(
-    //       focused.selectionStart, focused.selectionEnd);
-    //   } catch (err) {
-    //   }
-    // }
-    // if previous method did not work ask window for selection
-    if (selectedText == undefined) {
-      var currentSelection : Selection = window.getSelection();
-      var selectedText = currentSelection.toString();
-    }
-    if (selectedText) {
-      this.$log.debug('text that was selected: ' + selectedText);
-      return currentSelection;
-    } else {
-      return;
-    }
-  }
+    private findSelection () {
+      var focused : any = document.activeElement;
+      var selectedText : string;
+      // try grabbing text from an input or textarea field
+      // commenting this until we need to figure out tagging of editable fields
+      // if (focused) {
+      //   try {
+      //     selectedText = focused.value.substring(
+      //       focused.selectionStart, focused.selectionEnd);
+      //   } catch (err) {
+      //   }
+      // }
+      // if previous method did not work ask window for selection
+      if (selectedText == undefined) {
+        var currentSelection : Selection = window.getSelection();
+        var selectedText = currentSelection.toString();
+      }
+      if (selectedText) {
+        this.$log.debug('text that was selected: ' + selectedText);
+        return currentSelection;
+        } else {
+          return;
+        }
+      }
 }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -6,4 +6,3 @@
 /// <reference path="filesystem/filesystem.d.ts" />
 /// <reference path="filewriter/filewriter.d.ts" />
 /// <reference path="webrtc/MediaStream.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
