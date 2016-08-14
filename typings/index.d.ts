11a72724ce28769dd0bd75ffd0811110fefedb3d
Nils Norman Haukås <nils@thunki.com>
2016-08-14 18:40:02 +0200

    remove typings folder from version control

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/chrome/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/node-uuid/node-uuid-base/index.d.ts" />
-/// <reference path="globals/node-uuid/node-uuid-cjs/index.d.ts" />
-/// <reference path="globals/rangy/index.d.ts" />
