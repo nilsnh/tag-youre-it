305d0c24869334dbc16e5bd7dbca6553e4cd0a4c
Nils Norman Haukås <nils@thunki.com>
2017-06-06 20:19:52 +0200

    add karma test configuration

---
+++ src/app/services/test.spec.ts
@@ -0,0 +1,5 @@
+describe('my controller', function(){
+  it('runs a test', function(){
+    expect(0).toEqual(0);
+  })
+})
--- src/app/services/webpage.service.test.ts
+++ src/app/services/webpage.service.test.ts
@@ -1,7 +1,20 @@
+// describe('PasswordController', function() {
+//   beforeEach(module('app'));
 
-import test from 'ava'
-import { WebPageService } from './webpage.service'
+//   var $controller;
 
-test('readd tags to page', t => {
-  t.pass()
-})
+//   beforeEach(inject(function(_$controller_){
+//     // The injector unwraps the underscores (_) from around the parameter names when matching
+//     $controller = _$controller_;
+//   }));
+
+//   describe('$scope.grade', function() {
+//     it('sets the strength to "strong" if the password length is >8 chars', function() {
+//       var $scope = {};
+//       var controller = $controller('PasswordController', { $scope: $scope });
+//       $scope.password = 'longerthaneightchars';
+//       $scope.grade();
+//       expect($scope.strength).toEqual('strong');
+//     });
+//   });
+// });
