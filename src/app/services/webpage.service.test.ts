c1fec6cc544dc4a9b39808e661e356dace4de109
Nils Norman Haukås <nils@thunki.com>
2017-07-05 22:24:01 +0200

    more test tweaks. Refactor webpageService

--- src/app/services/webpage.service.test.ts
+++ src/app/services/webpage.service.test.ts
@@ -1,14 +1,23 @@
+import { WebPageService } from './webpage.service'
+
+declare var angular
 
 describe('WebPageService', function() {
   beforeEach(angular.mock.module('tagit'))
 
-  let webpageService;
-  beforeEach(inject(function(_WebPageService_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    webpageService = _WebPageService_;
-  }))
+  let webpageService: WebPageService
+  beforeEach(
+    inject(function(_WebPageService_) {
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+      webpageService = _WebPageService_
+    })
+  )
 
   it('should be able to instantiate service', function() {
-    expect(webpageService).toBeTruthy();
+    expect(webpageService).toBeTruthy()
+  })
+
+  it('deserializes tags', function() {
+    expect(webpageService.tryDeserializeTags([])).toBeTruthy()
   })
 })
--- src/app/services/webpage.service.ts
+++ src/app/services/webpage.service.ts
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
-import {TagStorageService} from './tagStorage.service';
-import {ISense, ISenseTag} from '../index.interfaces';
+import { TagStorageService } from './tagStorage.service'
+import { ISense, ISenseTag } from '../index.interfaces'
 
-import * as rangy from 'rangy';
-import 'rangy/lib/rangy-selectionsaverestore';
-import 'rangy/lib/rangy-serializer';
-import * as _ from 'lodash';
-import * as uuidV4 from 'uuid/v4';
+import * as rangy from 'rangy'
+import 'rangy/lib/rangy-selectionsaverestore'
+import 'rangy/lib/rangy-serializer'
+import * as _ from 'lodash'
+import * as uuidV4 from 'uuid/v4'
 
 /**
  * This service is responsible for interfacing with the content
  * that we want to tag. It injects/removes tags and listens for clicks.
  */
 export class WebPageService {
-
   // when clicking the menu to select a synset
   // we need to remember what the currently selected word was
-  savedSelection: Object;
-  savedText: string;
-  listOfFramesWithContent: (HTMLFrameElement | HTMLIFrameElement)[] = [];
+  savedSelection: Object
+  savedText: string
+  listOfFramesWithContent: (HTMLFrameElement | HTMLIFrameElement)[] = []
 
   constructor(
     private $log: ng.ILogService,
     private TagStorageService: TagStorageService,
-    private $rootScope: ng.IRootScopeService) {
-    rangy.init();
+    private $rootScope: ng.IRootScopeService
+  ) {
+    rangy.init()
   }
 
   /**
@@ -34,24 +34,24 @@ export class WebPageService {
    * was selected.
    */
   clickhandler = (evt: Event) => {
-    this.$log.debug('A click happened!');
-    var documentThatWasClicked = evt.srcElement.ownerDocument;
+    this.$log.debug('A click happened!')
+    var documentThatWasClicked = evt.srcElement.ownerDocument
     if (!documentThatWasClicked.hasFocus()) {
-      return true;
-    }
-    else if (wasRemoveTagButtonClicked(evt)) {
-      this.$log.debug('remove tag button was clicked');
-      removeTagFromWebAndStorage(evt, this.TagStorageService);
-    }
-    else if (documentThatWasClicked.getSelection().toString() !== '') {
-      resetSavedSelection(this.savedSelection);
-      this.savedSelection = rangy.saveSelection(documentThatWasClicked);
-      this.savedText = joinLongWords(documentThatWasClicked.getSelection().toString());
-      this.$rootScope.$broadcast('wordWasSelected', this.savedText);
+      return true
+    } else if (wasRemoveTagButtonClicked(evt)) {
+      this.$log.debug('remove tag button was clicked')
+      removeTagFromWebAndStorage(evt, this.TagStorageService)
+    } else if (documentThatWasClicked.getSelection().toString() !== '') {
+      resetSavedSelection(this.savedSelection)
+      this.savedSelection = rangy.saveSelection(documentThatWasClicked)
+      this.savedText = joinLongWords(
+        documentThatWasClicked.getSelection().toString()
+      )
+      this.$rootScope.$broadcast('wordWasSelected', this.savedText)
     } else {
-      resetSavedSelection(this.savedSelection);
-      this.savedText = '';
-      this.$rootScope.$broadcast('wordWasDeSelected', null);
+      resetSavedSelection(this.savedSelection)
+      this.savedText = ''
+      this.$rootScope.$broadcast('wordWasDeSelected', null)
     }
 
     /**
@@ -60,27 +60,30 @@ export class WebPageService {
      */
     function resetSavedSelection(savedSelection) {
       if (savedSelection) {
-        rangy.removeMarkers(savedSelection);
+        rangy.removeMarkers(savedSelection)
       }
     }
 
     function joinLongWords(possiblyLongWord: string) {
-      return possiblyLongWord.trim().split(" ").join("_");
+      return possiblyLongWord.trim().split(' ').join('_')
     }
 
     function wasRemoveTagButtonClicked(evt: any) {
-      return evt.target.className === 'js-tagit-remove-tag';
+      return evt.target.className === 'js-tagit-remove-tag'
     }
 
     //todo ensure removing a tag
-    function removeTagFromWebAndStorage(evt: Event, tagStorageService: TagStorageService) {
-      var target = <HTMLElement>evt.target;
-      var theOriginalTextNode = target.previousSibling;
-      var theSurroundingSpanElement = target.parentElement;
-      var parentElement = theSurroundingSpanElement.parentElement;
-      tagStorageService.deleteTagById(theSurroundingSpanElement.id);
-      parentElement.replaceChild(theOriginalTextNode, theSurroundingSpanElement);
-      parentElement.normalize();
+    function removeTagFromWebAndStorage(
+      evt: Event,
+      tagStorageService: TagStorageService
+    ) {
+      var target = <HTMLElement>evt.target
+      var theOriginalTextNode = target.previousSibling
+      var theSurroundingSpanElement = target.parentElement
+      var parentElement = theSurroundingSpanElement.parentElement
+      tagStorageService.deleteTagById(theSurroundingSpanElement.id)
+      parentElement.replaceChild(theOriginalTextNode, theSurroundingSpanElement)
+      parentElement.normalize()
     }
   }
 
@@ -89,19 +92,22 @@ export class WebPageService {
    * We then place click listeners on each of the frames.
    */
   addListenersToPage() {
-    const tagitBodyIframe = <HTMLIFrameElement>parent.document.getElementById('tagit-body');
-    const tagitBodyIframeDoc = tagitBodyIframe.contentDocument;
+    const tagitBodyIframe = <HTMLIFrameElement>parent.document.getElementById(
+      'tagit-body'
+    )
+    const tagitBodyIframeDoc = tagitBodyIframe.contentDocument
 
     if (tagitBodyIframeDoc.getElementsByTagName('frameset').length > 0) {
-      _.map(tagitBodyIframeDoc.getElementsByTagName('frame'),
-        (frame) => { this.listOfFramesWithContent.push(frame) });
+      _.map(tagitBodyIframeDoc.getElementsByTagName('frame'), frame => {
+        this.listOfFramesWithContent.push(frame)
+      })
     } else {
-      this.listOfFramesWithContent.push(tagitBodyIframe);
+      this.listOfFramesWithContent.push(tagitBodyIframe)
     }
 
     _.map(this.listOfFramesWithContent, (frame: HTMLIFrameElement) => {
-      frame.contentDocument.addEventListener('click', this.clickhandler, false);
-    });
+      frame.contentDocument.addEventListener('click', this.clickhandler, false)
+    })
   }
 
   /**
@@ -113,29 +119,33 @@ export class WebPageService {
     //loop that will keep asking for spans to remove
     //until they are all gone.
     const done = _.after(this.listOfFramesWithContent.length, () => {
-      callback();
-    });
+      callback()
+    })
 
-    _.map(this.listOfFramesWithContent, (iframe) => {
-      removeTagsFromIframe(iframe, done);
-    });
+    _.map(this.listOfFramesWithContent, iframe => {
+      removeTagsFromIframe(iframe, done)
+    })
 
-    function removeTagsFromIframe(iframe: HTMLFrameElement | HTMLIFrameElement,
-      callback: () => void) {
-      while (iframe.contentDocument.getElementsByClassName('tagit-tag').length > 0) {
-        var spanElement = iframe.contentDocument.getElementsByClassName('tagit-tag')[0];
-        var spanElementParent = spanElement.parentNode;
-        spanElementParent.replaceChild(
-          spanElement.firstChild,
-          spanElement);
+    function removeTagsFromIframe(
+      iframe: HTMLFrameElement | HTMLIFrameElement,
+      callback: () => void
+    ) {
+      while (
+        iframe.contentDocument.getElementsByClassName('tagit-tag').length > 0
+      ) {
+        var spanElement = iframe.contentDocument.getElementsByClassName(
+          'tagit-tag'
+        )[0]
+        var spanElementParent = spanElement.parentNode
+        spanElementParent.replaceChild(spanElement.firstChild, spanElement)
         /**
          * call normalize on the parent element so that it will join up
          * any text nodes that might have become chopped up by
          * tags and selection markers.
          *  */
-        spanElementParent.normalize();
+        spanElementParent.normalize()
       }
-      callback();
+      callback()
     }
   }
 
@@ -144,46 +154,23 @@ export class WebPageService {
    * bottom up order, so that all the tags find their right place.
    */
   readdTagsToPage(tagsToLoad: ISenseTag[]) {
-    this.$log.debug('readdTagsToPage()');
+    this.$log.debug('readdTagsToPage()')
 
     //first deselect all places before we go to work
-    this.removeAllRanges();
+    this.removeAllRanges()
 
-    /**
-     * The tags need to be loaded (deserialized) so that
-     * they can be inserted properly. Deserialization might
-     * fail if the page has changed since it was tagged. Thus
-     * we remove tags that fail to load.
-     */
-    tagsToLoad = _.filter(tagsToLoad, (tagToLoad) => {
-      try {
-        tagToLoad.deserializedRange = rangy.deserializeRange(
-          tagToLoad.serializedSelectionRange,
-          this.listOfFramesWithContent[tagToLoad.iframeIndex].contentDocument.documentElement,
-          this.listOfFramesWithContent[tagToLoad.iframeIndex]
-        );
-        return true;
-      } catch (e) {
-        var errorMsg = `Error in rangy.js: Was not able to deserialize range.
-            In other words: The page might have changed. Is not able
-            to determine where this tag should have been placed.`
-        this.$log.debug(errorMsg);
-        this.$log.debug("Couldn't load: " + tagToLoad.sense.word);
-        this.$log.debug(e);
-        return false;
-      }
-    })
+    tagsToLoad = this.tryDeserializeTags(tagsToLoad)
 
-    this.$log.debug('finished deserializing tags');
+    this.$log.debug('finished deserializing tags')
 
     /**
      * sort tags descending (highest number = furthest down on page).
      */
     tagsToLoad = _.sortBy(tagsToLoad, (tag: ISenseTag) => {
-      return tag.deserializedRange.startOffset;
-    });
+      return tag.deserializedRange.startOffset
+    })
 
-    this.$log.debug('finished sorting tags');
+    this.$log.debug('finished sorting tags')
 
     /**
      * Loop through loaded tags and insert them to the page.
@@ -194,38 +181,75 @@ export class WebPageService {
           this.listOfFramesWithContent[tag.iframeIndex].contentDocument,
           tag.sense,
           tag.deserializedRange,
-          tag.id);
+          tag.id
+        )
       }
-    });
+    })
 
-    this.$log.debug('finished adding tags to page');
-    this.removeAllRanges();
+    this.$log.debug('finished adding tags to page')
+    this.removeAllRanges()
+  }
+
+  /**
+   * The tags need to be loaded (deserialized) so that
+   * they can be inserted properly. Deserialization might
+   * fail if the page has changed since it was tagged. Thus
+   * we remove tags that fail to load.
+   */
+  tryDeserializeTags = (tags: ISenseTag[]): ISenseTag[] => {
+    return _.filter(tags, tagToLoad => {
+      try {
+        tagToLoad.deserializedRange = rangy.deserializeRange(
+          tagToLoad.serializedSelectionRange,
+          this.listOfFramesWithContent[tagToLoad.iframeIndex].contentDocument
+            .documentElement,
+          this.listOfFramesWithContent[tagToLoad.iframeIndex]
+        )
+        return true
+      } catch (e) {
+        var errorMsg = `Error in rangy.js: Was not able to deserialize range.
+            In other words: The page might have changed. Is not able
+            to determine where this tag should have been placed.`
+        this.$log.debug(errorMsg)
+        this.$log.debug("Couldn't load: " + tagToLoad.sense.word)
+        this.$log.debug(e)
+        return false
+      }
+    })
   }
 
   initializeNewTag = (sense: ISense): ISenseTag => {
-    this.$log.debug('initializeNewTag');
+    this.$log.debug('initializeNewTag')
 
     /**
      * first eliminate all selections to avoid confusion
      * with other iframes.
      */
-    this.removeAllRanges();
-    rangy.restoreSelection(this.savedSelection);
-    var iframeOfInterest = getFrameContainingSelection(this.listOfFramesWithContent);
-    const selection = iframeOfInterest.contentDocument.getSelection();
+    this.removeAllRanges()
+    rangy.restoreSelection(this.savedSelection)
+    var iframeOfInterest = getFrameContainingSelection(
+      this.listOfFramesWithContent
+    )
+    const selection = iframeOfInterest.contentDocument.getSelection()
 
-    var range: Range = selection.getRangeAt(0);
-    var serializedRange = rangy.serializeRange(range, true,
-      iframeOfInterest.contentDocument.documentElement);
-    var generatedUuid: string = uuidV4();
-    var parentElement = <HTMLElement>range.commonAncestorContainer;
+    var range: Range = selection.getRangeAt(0)
+    var serializedRange = rangy.serializeRange(
+      range,
+      true,
+      iframeOfInterest.contentDocument.documentElement
+    )
+    var generatedUuid: string = uuidV4()
+    var parentElement = <HTMLElement>range.commonAncestorContainer
 
     return {
       id: generatedUuid,
       userEmail: 'testEmail',
       sense: sense,
       wordThatWasTagged: selection.toString(),
-      iframeIndex: getIframeIndex(this.listOfFramesWithContent, iframeOfInterest),
+      iframeIndex: getIframeIndex(
+        this.listOfFramesWithContent,
+        iframeOfInterest
+      ),
       context: parentElement.textContent,
       serializedSelectionRange: serializedRange,
       urlOfPageThatWasTagged: window.location.href
@@ -236,46 +260,52 @@ export class WebPageService {
      * need to loop through them, identify which document contains the current
      * selection and then save its index in our list.
      */
-    function getIframeIndex(iframeList: (HTMLFrameElement | HTMLIFrameElement)[],
-      iframeToFind: (HTMLFrameElement | HTMLIFrameElement)) {
+    function getIframeIndex(
+      iframeList: (HTMLFrameElement | HTMLIFrameElement)[],
+      iframeToFind: HTMLFrameElement | HTMLIFrameElement
+    ) {
       for (var i = 0; i < iframeList.length; i++) {
-        var iframeInlist = iframeList[i];
+        var iframeInlist = iframeList[i]
         if (iframeInlist === iframeToFind) {
-          return i;
+          return i
         }
       }
-      return 0;
+      return 0
     }
 
-    function getFrameContainingSelection(iframeList: (HTMLFrameElement | HTMLIFrameElement)[]) {
-      return _.find(iframeList, (frame) => {
-        return frame.contentDocument.getSelection().toString() !== '';
-      });
+    function getFrameContainingSelection(
+      iframeList: (HTMLFrameElement | HTMLIFrameElement)[]
+    ) {
+      return _.find(iframeList, frame => {
+        return frame.contentDocument.getSelection().toString() !== ''
+      })
     }
-
-  };
-
-  private removeAllRanges = () => {
-    _.map(this.listOfFramesWithContent, (iframe) => {
-      iframe.contentDocument.getSelection().removeAllRanges();
-    });
   }
 
-  private surroundRangeWithSpan(documentToManipulate: HTMLDocument,
-    sense: ISense, range: Range, uuid: string) {
+  private removeAllRanges = () => {
+    _.map(this.listOfFramesWithContent, iframe => {
+      iframe.contentDocument.getSelection().removeAllRanges()
+    })
+  }
 
+  private surroundRangeWithSpan(
+    documentToManipulate: HTMLDocument,
+    sense: ISense,
+    range: Range,
+    uuid: string
+  ) {
     // add span around content
-    var span: HTMLSpanElement = documentToManipulate.createElement('span');
-    span.id = uuid;
-    span.title = sense.explanation;
-    span.className = 'tagit-tag';
+    var span: HTMLSpanElement = documentToManipulate.createElement('span')
+    span.id = uuid
+    span.title = sense.explanation
+    span.className = 'tagit-tag'
 
-    range.surroundContents(span);
+    range.surroundContents(span)
 
     // add a button for removing the tag.
-    var btn = documentToManipulate.createElement("BUTTON");
-    btn.className = 'js-tagit-remove-tag';
-    btn.appendChild(documentToManipulate.createTextNode("X"));
-    span.appendChild(btn);
+    var btn = documentToManipulate.createElement('BUTTON')
+    btn.className = 'js-tagit-remove-tag'
+    btn.appendChild(documentToManipulate.createTextNode('X'))
+    span.appendChild(btn)
   }
 }
