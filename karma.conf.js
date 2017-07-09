95fa6a67a6f53344e479200652cec806d559fd8e
Nils Norman Haukås <nils@thunki.com>
2017-07-09 17:09:35 +0200

    formatting adjustment

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,13 @@
 // Karma configuration
 // Generated on Tue Jun 06 2017 20:05:31 GMT+0200 (CEST)
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'jasmine', 'karma-typescript' ],
+    frameworks: ['jasmine', 'karma-typescript'],
     // list of files / patterns to load in the browser
     files: [
       'karma.pre-test.js',
@@ -19,17 +19,17 @@ module.exports = function (config) {
     exclude: [],
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { '**/*.ts': [ 'karma-typescript' ] },
+    preprocessors: { '**/*.ts': ['karma-typescript'] },
     karmaTypescriptConfig: {
       bundlerOptions: {
-        transforms: [ require('karma-typescript-es6-transform')() ]
+        transforms: [require('karma-typescript-es6-transform')()]
       },
       tsconfig: './tsconfig.json'
     },
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'progress', 'karma-typescript' ],
+    reporters: ['progress', 'karma-typescript'],
     // web server port
     port: 9876,
     // enable / disable colors in the output (reporters and logs)
@@ -41,7 +41,7 @@ module.exports = function (config) {
     autoWatch: true,
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'Chrome' ],
+    browsers: ['Chrome'],
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
